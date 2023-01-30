c14f1e8b596cb6757f705c75a1cec9a455050c93
Cameron Bose <cameron.bose@fwdview.com>
2023-01-30 14:54:52 +0000

    update POST

--- POST_Commands.sh
+++ POST_Commands.sh
@@ -99,7 +99,7 @@ authKey=$(echo $authKey | cut -d ":" -f 2 | tr -d '",}')
 if [ ${engineType} == "MASKING" ];then
     echo "then this bit ${authKey}"
     curl -s -X PUT -k --data @- http://${dxEngine}/masking/api/users/6 \
-    -b "cookies.txt" -c "cookies.txt" -H "Content-Type: application/json" -H "Authorization: Basic ${authKey}" <<EOF
+    -b "cookies.txt" -c "cookies.txt" -H "Content-Type: application/json" -H "Authorization: ${authKey}" <<EOF
     {"userName":"admin","password":"${password}","firstName":"","lastName":"","email":"${email}","isAdmin":true,"userStatus":"ACTIVE"}
 EOF
 fi 
